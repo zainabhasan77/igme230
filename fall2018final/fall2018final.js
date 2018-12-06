31dd5667143004ceba69d1845076dca92d9a9380
Zainab Hasan <zainabhasan@zainabs-mbp.wireless.rit.edu>
2018-12-06 18:15:51 -0500

    Final

---
+++ fall2018final/fall2018final.js
@@ -0,0 +1,26 @@
+$(".menu").click(function () {
+    $(this).next(".items").slideToggle();
+});
+
+
+
+
+let selection = ("article0.txt")
+
+$("article").val(selection);
+$("article").load(selection);
+
+
+$('input[type=radio][name=article]').change(function () {
+            selection = $(this).val();
+            $("article").load(selection);
+
+
+
+
+
+            let clicks = 0;
+            $("#clickme").click(function () {
+                clicks++;
+                $("#count").html(clicks);
+            })
